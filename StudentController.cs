f1a1f01c1b55958dd7f25fadedbe2cc151f01102
Jahaira12 <82242646+Jahaira12@users.noreply.github.com>
2024-12-01 22:03:01 -0700

    Add files via upload

---
+++ StudentController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic; // Required for IEnumerable and List
+
+namespace serviceRest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Student> GetStudentName()
+        {
+            return new List<Student>
+            {
+                new Student
+                {
+                    Id = 123,
+                    StudentName = "James"
+                },
+                new Student
+                {
+                    Id = 124,
+                    StudentName = "Jill"
+                }
+            };
+        }
+    }
+
+    public class Student
+    {
+        public int Id { get; set; } // Corrected set syntax
+        public string StudentName { get; set; } // Corrected set syntax
+    }
+}
---
+++ ValuesController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace serviceRest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+    }
+}
